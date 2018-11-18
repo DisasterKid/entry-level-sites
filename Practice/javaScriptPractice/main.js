d359a050e9cea8d1b95d96ce03ebc7fcb44e91dc
Jon <stamen37@gmail.com>
2018-11-18 10:12:21 +0800

    stuff

---
+++ Practice/javaScriptPractice/main.js
@@ -0,0 +1,20 @@
+const companies = [
+    {name: "Company one", category: "Finance", start: 1981, end: 2003},
+    {name: "Company two", category: "Retail", start: 1992, end: 2008},
+    {name: "Company three", category: "Auto", start: 1999, end: 2007},
+    {name: "Company four", category: "Retail", start: 1989, end: 2010},
+    {name: "Company five", category: "Technology", start: 2009, end: 2014},
+    {name: "Company six", category: "Finance", start: 1987, end: 2010}
+];
+
+
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+//console.log(companies[0]);
+
+for (let i = 0; i < companies.length; i++){
+    alert(companies) ;
+}
+
+
